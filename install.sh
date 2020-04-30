463b669f1b6ca9a15c7af3579086943683c33ef5
Vignesh Burugina <vignesh.student4@gmail.com>
2020-04-30 12:34:45 +0530

    Revision

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ echo "Please run this as Superuser"
 echo "Installing Alpha"
 echo "Build-ver0.1"
 mkdir ~/Documents/Invoices
-sudo mv ./build-files/create-invoice.sh /bin
+sudo mv ./build-files/create-invoice /bin
+sudo chmod +x /bin/create-invoice
 sudo mv ./build-files/Invoice.pdf ~/Templates
 sudo mv ./build-files/invoice.py /bin
 echo 'Finished Copying Files'
